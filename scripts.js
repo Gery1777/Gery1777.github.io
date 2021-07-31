0963cdd702d8f4127d7d197c1e25dec2f59760b3
Gery1777 <kovacs.gery@hotmail.hu>
2021-07-31 19:35:14 +0200

    Complete change of content - ToDoList

---
+++ scripts.js
@@ -0,0 +1,84 @@
+//getting inputs
+const inputbox = document.getElementById("txtAddListItem");
+const addbutton = document.getElementById("btnAddListItem");
+const todolist = document.querySelector(".list")
+
+inputbox.onkeyup = () => {
+    let input = inputbox.value;
+    if (input.trim() != 0) {
+        addbutton.classList.add("active")
+    } else {
+        addbutton.classList.remove("active")
+    }
+}
+showlist();
+
+addbutton.onclick = () => {
+    let input = inputbox.value
+    if (input.trim() != 0) {
+        let getlocalstorage = localStorage.getItem("New Item")
+        if (getlocalstorage == null) {
+            listitems = [];
+        } else {
+            listitems = JSON.parse(getlocalstorage);
+        }
+        listitems.push(input)
+        localStorage.setItem("New Item", JSON.stringify(listitems));
+        addbutton.classList.remove("active")
+        showlist();
+        write_footer();
+    }
+}
+
+//add task list inside ul
+function showlist() {
+    let input = inputbox.value
+    let getlocalstorage = localStorage.getItem("New Item")
+    if (getlocalstorage == null) {
+        listitems = [];
+    } else {
+        listitems = JSON.parse(getlocalstorage);
+    }
+    let new_row = "";
+
+    listitems.forEach((element, index) => {
+        new_row += `<li> ${element} <span><i class="fas fa-trash" onclick=deletetask(${index})></i></span></li>`
+    });
+    todolist.innerHTML = new_row;
+    inputbox.value = "";
+}
+
+//delete task function
+function deletetask(index) {
+    let getlocalstorage = localStorage.getItem("New Item")
+    listitems = JSON.parse(getlocalstorage);
+    listitems.splice(index, 1);
+    //update the list after delete is done
+    localStorage.setItem("New Item", JSON.stringify(listitems));
+    showlist();
+    write_footer();
+}
+
+
+function clearall() {
+    localStorage.clear();
+    showlist();
+    let footer = document.querySelector(".footer")
+    footer.innerHTML = (`<i>You have 0 items on your list.</i>  <button ID="btnClearAll" OnClick=clearall();> Clear All</button>`)
+}
+
+function write_footer() {
+    let getlocalstorage = localStorage.getItem("New Item")
+    listitems = JSON.parse(getlocalstorage)
+
+    let rowcount = 0;
+
+    listitems.forEach(element => {
+        rowcount += 1
+    });
+    let footer = document.querySelector(".footer")
+    footer.innerHTML = (`<i>You have ${rowcount} items on your list.</i>  <button ID="btnClearAll" OnClick=clearall();> Clear All</button>`)
+}
+write_footer();
+
+
